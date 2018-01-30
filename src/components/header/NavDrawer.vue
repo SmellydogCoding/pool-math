5243745d65e68f92d2abb19d3265ddbe73af0f8b
SmellydogCoding <SmellydogCoding@gmail.com>
2018-01-29 21:33:23 -0500

    modulazation for the nav drawer

--- src/components/header/NavDrawer.vue
+++ src/components/header/NavDrawer.vue
@@ -1,87 +1,74 @@
 <template lang="pug">
   div
     v-toolbar-side-icon(@click="drawer = !drawer")
-    v-navigation-drawer(v-model="drawer" dark fixed temporary width="350")
+    v-navigation-drawer(v-model="drawer" dark fixed temporary :width="width")
       v-list.pa-1
         v-list-tile(avatar tag="div")
           v-list-tile-avatar
-            img(src="../../../public/v.png")
+            img(:src="avatar")
           v-list-tile-content
-            v-list-tile-title Guest
+            v-list-tile-title {{ title }}
           v-list-tile-action
             v-btn(icon @click="drawer = !drawer")
               v-icon close
       v-list.pt-0(dense)
         v-divider(light)
-        router-link(to="/howto" tag="v-list-tile" active-class="active").expansion-panel__header.how-to
+        router-link.how-to.py-2(to="/howto" tag="v-list-tile" active-class="active")
           v-list-tile-content
-            v-list-tile-title.text-xs-center How to Use This Website
+            v-list-tile-title.subheading.text-xs-center How to Use This Website
         v-expansion-panel
-          v-expansion-panel-content
-            div(slot="header") Section 1 - Conversions
-            router-link(to="/s1p0" tag="v-list-tile" active-class="active")
+          v-expansion-panel-content(v-for="section in menu" :key="section.title")
+            div(slot="header") {{ section.title }}
+            router-link(:to="subsection.link" tag="v-list-tile" active-class="active" v-for="subsection in section.subsections" :key="subsection.link")
               v-list-tile-content
-                v-list-tile-title 1.0 - About This Section
-            router-link(to="/s1p1" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                v-list-tile-title 1.1 - Convert ounces to pounds
-            router-link(to="/s1p2" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                v-list-tile-title 1.2 - Convert fluid ounces to gallons
-            router-link(to="/s1p3" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                v-list-tile-title 1.3 - Convert fluid ounces to cups
-            router-link(to="/s1p4" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                v-list-tile-title 1.4 - Convert yards to feet
-            router-link(to="/s1p4" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                v-list-tile-title 1.5 - Convert inches to feet
-          v-expansion-panel-content
-            div(slot="header") Section 2 - Area and Volume
-            router-link(to="/s2p0" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.0 - Introduction to This Section
-            router-link(to="/s2p1" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.1 - Area of a Swimming Pool
-            router-link(to="/s2p2" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.2 - Area of a Hot Tub
-            router-link(to="/s2p3" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.3 - Volume of Water in a Swimming Pool
-            router-link(to="/s2p4" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.4 - Volume of Water in a Multi-Depth Pool
-            router-link(to="/s2p5" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.5 - Volume of Water in a Hot Tub
-            router-link(to="/s2p6" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.6 - Volume of Water in a Hot Tub with Seats
-            router-link(to="/s2p7" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.7 - Volume of Water Loss in a Swimming Pool
-            router-link(to="/s2p8" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 2.8 - Volume of Water Loss in a Hot Tub
-          v-expansion-panel-content
-            div(slot="header") Section 3 - Water Balance
-            router-link(to="/s3p0" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 3.0 - Introduction to This Section
-            router-link(to="/s3p1" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 3.1 - Calculate Saturation Index
-            router-link(to="/s3p2" tag="v-list-tile" active-class="active")
-              v-list-tile-content
-                  v-list-tile-title 3.2 - Calculate Saturation Index - With CA
+                v-list-tile-title {{ subsection.title }}
 </template>
 
 <script>
 export default {
-  data() { return { drawer: false } }
+  data() {
+    return {
+      drawer: false,
+      width: '350',
+      avatar: '../../../public/buoy.jpg',
+      title: 'Menu',
+      menu: [
+        {
+          title: 'Section 1 - Conversions',
+          subsections: [
+            {link: '/s1p0', title: '1.0 - About This Section'},
+            {link: '/s1p1', title: '1.1 - Convert Ounces to Pounds'},
+            {link: '/s1p2', title: '1.2 - Convert Fluid Ounces to Gallons'},
+            {link: '/s1p3', title: '1.3 - Convert Fluid Ounces to Cups'},
+            {link: '/s1p4', title: '1.4 - Convert Yards to Feet'},
+            {link: '/s1p5', title: '1.5 - Convert Inches to Feet'}
+          ]
+        },
+        {
+          title: 'Section 2 - Area and Volume',
+          subsections: [
+            {link: '/s2p0', title: '2.0 - About This Section'},
+            {link: '/s2p1', title: '2.1 - Area of a Swimming Pool'},
+            {link: '/s2p2', title: '2.2 - Area of a Hot Tub'},
+            {link: '/s2p3', title: '2.3 - Volume of Water in a Swimming Pool'},
+            {link: '/s2p4', title: '2.4 - Volume of Water in a Multi-Depth Pool'},
+            {link: '/s2p5', title: '2.5 - Volume of Water in a Hot Tub'},
+            {link: '/s2p6', title: '2.6 - Volume of Water in a Hot Tub With Seats'},
+            {link: '/s2p7', title: '2.7 - Volume of Water Loss in a Swimming Pool'},
+            {link: '/s2p7', title: '2.8 - Volume of Water Loss in a Hot Tub'}
+          ]
+        },
+        {
+          title: 'Section 3 - Water Balance',
+          subsections: [
+            {link: '/s3p0', title: '3.0 - About This Section'},
+            {link: '/s3p1', title: '3.1 - Calculate Saturation Index'},
+            {link: '/s3p2', title: '3.2 - Calculate Saturation Index - With CA'}
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -89,13 +76,9 @@ export default {
   aside {
     overflow-y: auto;
   }
-  .navigation-drawer {
-    padding: 0 0 1.0rem;
-  }
+ 
   .how-to {
     border-bottom: 1px solid rgba(255,255,255,0.12);
   }
-  .how-to .list__tile__title {
-    font-size: 1.15rem;
-  }
+  
 </style>
