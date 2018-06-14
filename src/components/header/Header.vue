04b66af935529482429a132499078a43d27bc605
SmellydogCoding <SmellydogCoding@gmail.com>
2018-06-13 21:50:01 -0400

    contact me button

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -2,6 +2,9 @@
   v-toolbar.mt-0(dark)
     app-NavDrawer
     router-link(to="/" tag="v-toolbar-title") Pool Math
+    v-spacer
+    v-toolbar-items
+      v-btn.button--default-size(color="success" :href="`mailto:smellydogcoding@gmail.com`") Contact Me
 </template>
 
 <script>
@@ -18,4 +21,6 @@ export default {
   .toolbar__title {
     cursor: pointer;
   }
+  .toolbar__items { align-items: center; }
+  .button--default-size { height: 36px; }
 </style>
